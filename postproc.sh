8aa1b5339997c05a93cab2b4900f2436ecce1fe4
DoxasticFox <doxasticfox@github.com>
2017-08-04 19:26:44 +1000

    Finished module

---
+++ postproc.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+cd module
+
+# Add Select_User dropdown to search
+string="
+        <select1 ref=\"Entity_Types\">
+          <label>{Entity_Types}<\/label>
+          <item>
+            <label>Options not loaded<\/label>
+            <value>Options not loaded<\/value>
+          <\/item>
+        <\/select1>"
+replacement="
+        <group ref=\"Colgroup_1\" faims_style=\"orientation\">
+          <label\/>
+          <group ref=\"Col_0\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Entity_Types\">
+              <label>{Entity_Types}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+          <group ref=\"Col_1\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Select_User\">
+              <label>{Select_User}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+        <\/group>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+string="
+              <Entity_Types\/>"
+replacement="
+              <Colgroup_1>
+                <Col_0>
+                  <Entity_Types\/>
+                <\/Col_0>
+                <Col_1>
+                  <Select_User\/>
+                <\/Col_1>
+              <\/Colgroup_1>
+              <Select_User\/>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+# Add Create_Shape button to map
+string="
+        <trigger ref=\"Center_Me_1\">
+          <label>{Center_Me}<\/label>
+        <\/trigger>"
+replacement="
+        <group ref=\"Colgroup_A\" faims_style=\"orientation\">
+          <label\/>
+          <group ref=\"Col_0\" faims_style=\"even\">
+            <label\/>
+            <trigger ref=\"Center_Me_1\">
+              <label>{Center_Me}<\/label>
+            <\/trigger>
+          <\/group>
+          <group ref=\"Col_1\" faims_style=\"even\">
+            <label\/>
+            <trigger ref=\"Create_Shape\">
+              <label>{Create_Shape}<\/label>
+            <\/trigger>
+          <\/group>
+        <\/group>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+string="
+              <Center_Me_1\/>"
+replacement="
+              <Colgroup_A>
+                <Col_0>
+                  <Center_Me_1\/>
+                <\/Col_0>
+                <Col_1>
+                  <Create_Shape\/>
+                <\/Col_1>
+              <\/Colgroup_A>
+              <Select_User\/>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+
+rm ui_schema.xml.original
+
+cat << EOF >> ui_styling.css
+.readonly {
+  color: #B2B2B2;
+}
+EOF
+
+cat << EOF >> english.0.properties
+All=All
+Create_Shape=Create Shape
+Select_User=Select User
+EOF
--- tests/ModuleUtil.java
+++ tests/ModuleUtil.java
@@ -17,21 +17,39 @@ public class ModuleUtil extends ModuleHelper {
 
     /*
         Type: tab group
-        Ref:  Simple_Form
+        Ref:  Feature
      */
-    public static View get_SimpleForm(Solo solo) {
-        String ref = "Simple_Form";
+    public static View get_Feature(Solo solo) {
+        String ref = "Feature";
         return (android.view.View) solo.getView((Object) ref);
     }
 
     /*
         Type: tab group
-        Ref:  User
+        Ref:  Shape
      */
-    public static View get_User(Solo solo) {
-        String ref = "User";
+    public static View get_Shape(Solo solo) {
+        String ref = "Shape";
         return (android.view.View) solo.getView((Object) ref);
     }
+
+    /*
+        Type: tab group
+        Ref:  User_List
+     */
+    public static View get_UserList(Solo solo) {
+        String ref = "User_List";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+    /*
+        Type: tab
+        Ref:  Control/IDs
+     */
+    public static View get_Control_IDs(Solo solo) {
+        String ref = "Control/IDs";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
     /*
         Type: tab
         Ref:  Control/Map
@@ -43,30 +61,75 @@ public class ModuleUtil extends ModuleHelper {
 
     /*
         Type: tab
-        Ref:  Control/Simple_Forms
+        Ref:  Control/Vars
      */
-    public static View get_Control_SimpleForms(Solo solo) {
-        String ref = "Control/Simple_Forms";
+    public static View get_Control_Vars(Solo solo) {
+        String ref = "Control/Vars";
         return (android.view.View) solo.getView((Object) ref);
     }
 
     /*
         Type: tab
-        Ref:  Simple_Form/Simple_Form
+        Ref:  Feature/GControl
      */
-    public static View get_SimpleForm_SimpleForm(Solo solo) {
-        String ref = "Simple_Form/Simple_Form";
+    public static View get_Feature_GControl(Solo solo) {
+        String ref = "Feature/GControl";
         return (android.view.View) solo.getView((Object) ref);
     }
 
     /*
         Type: tab
-        Ref:  User/User
+        Ref:  Feature/Main
      */
-    public static View get_User_User(Solo solo) {
-        String ref = "User/User";
+    public static View get_Feature_Main(Solo solo) {
+        String ref = "Feature/Main";
         return (android.view.View) solo.getView((Object) ref);
     }
+
+    /*
+        Type: tab
+        Ref:  Shape/Shape_Note
+     */
+    public static View get_Shape_ShapeNote(Solo solo) {
+        String ref = "Shape/Shape_Note";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  User_List/Manual
+     */
+    public static View get_UserList_Manual(Solo solo) {
+        String ref = "User_List/Manual";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  User_List/User_List
+     */
+    public static View get_UserList_UserList(Solo solo) {
+        String ref = "User_List/User_List";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+    /*
+        Type: button
+        Ref:  Control/IDs/Create_Feature
+     */
+    public static View get_Control_IDs_CreateFeature(Solo solo) {
+        String ref = "Control/IDs/Create_Feature";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Control/Map/Assign_Colour
+     */
+    public static View get_Control_Map_AssignColour(Solo solo) {
+        String ref = "Control/Map/Assign_Colour";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
     /*
         Type: button
         Ref:  Control/Map/Center_Me_1
@@ -78,109 +141,307 @@ public class ModuleUtil extends ModuleHelper {
 
     /*
         Type: button
-        Ref:  Control/Simple_Forms/New_Simple_Form
+        Ref:  Control/Search/Search_Button
      */
-    public static View get_Control_SimpleForms_NewSimpleForm(Solo solo) {
-        String ref = "Control/Simple_Forms/New_Simple_Form";
+    public static View get_Control_Search_SearchButton(Solo solo) {
+        String ref = "Control/Search/Search_Button";
         return (android.view.View) solo.getView((Object) ref);
     }
 
     /*
         Type: button
-        Ref:  Simple_Form/Simple_Form/Take_From_GPS_1
+        Ref:  Control/Vars/Create_Shape
      */
-    public static View get_SimpleForm_SimpleForm_TakeFromGPS1(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/Take_From_GPS_1";
+    public static View get_Control_Vars_CreateShape(Solo solo) {
+        String ref = "Control/Vars/Create_Shape";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Feature/GControl/Picture_Button_1
+     */
+    public static View get_Feature_GControl_PictureButton1(Solo solo) {
+        String ref = "Feature/GControl/Picture_Button_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Feature/Main/Find_This_in_The_Map
+     */
+    public static View get_Feature_Main_FindThisinTheMap(Solo solo) {
+        String ref = "Feature/Main/Find_This_in_The_Map";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Feature/Main/Take_From_GPS_1
+     */
+    public static View get_Feature_Main_TakeFromGPS1(Solo solo) {
+        String ref = "Feature/Main/Take_From_GPS_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: camera
+        Ref:  Feature/GControl/Picture
+     */
+    public static View get_Feature_GControl_Picture(Solo solo) {
+        String ref = "Feature/GControl/Picture";
         return (android.view.View) solo.getView((Object) ref);
     }
 
     /*
         Type: dropdown
-        Ref:  Simple_Form/Simple_Form/Dropdown
+        Ref:  Control/Map/Colour_to_Assign
      */
-    public static View get_SimpleForm_SimpleForm_Dropdown(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/Dropdown";
+    public static View get_Control_Map_ColourtoAssign(Solo solo) {
+        String ref = "Control/Map/Colour_to_Assign";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Control/Map/GC_Value
+     */
+    public static View get_Control_Map_GCValue(Solo solo) {
+        String ref = "Control/Map/GC_Value";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Control/Search/Entity_Types
+     */
+    public static View get_Control_Search_EntityTypes(Solo solo) {
+        String ref = "Control/Search/Entity_Types";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Feature/GControl/Classification
+     */
+    public static View get_Feature_GControl_Classification(Solo solo) {
+        String ref = "Feature/GControl/Classification";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Feature/GControl/GC
+     */
+    public static View get_Feature_GControl_GC(Solo solo) {
+        String ref = "Feature/GControl/GC";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Feature/Main/Feature_Type
+     */
+    public static View get_Feature_Main_FeatureType(Solo solo) {
+        String ref = "Feature/Main/Feature_Type";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Feature/Main/Source
+     */
+    public static View get_Feature_Main_Source(Solo solo) {
+        String ref = "Feature/Main/Source";
         return (android.view.View) solo.getView((Object) ref);
     }
 
     /*
         Type: input
-        Ref:  Simple_Form/Simple_Form/Accuracy
+        Ref:  Control/IDs/Next_Line_ID
      */
-    public static View get_SimpleForm_SimpleForm_Accuracy(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/Accuracy";
-        return (android.view.View) solo.getView((Object) ref);
-    }
-
-    /*
-        Type: input
-        Ref:  Simple_Form/Simple_Form/Easting
-     */
-    public static View get_SimpleForm_SimpleForm_Easting(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/Easting";
-        return (android.view.View) solo.getView((Object) ref);
-    }
-
-    /*
-        Type: input
-        Ref:  Simple_Form/Simple_Form/ID
-     */
-    public static EditText get_SimpleForm_SimpleForm_ID(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/ID";
+    public static EditText get_Control_IDs_NextLineID(Solo solo) {
+        String ref = "Control/IDs/Next_Line_ID";
         return (android.widget.EditText) solo.getView((Object) ref);
     }
 
     /*
         Type: input
-        Ref:  Simple_Form/Simple_Form/Latitude
+        Ref:  Control/IDs/Next_Point_ID
      */
-    public static View get_SimpleForm_SimpleForm_Latitude(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/Latitude";
-        return (android.view.View) solo.getView((Object) ref);
-    }
-
-    /*
-        Type: input
-        Ref:  Simple_Form/Simple_Form/Longitude
-     */
-    public static View get_SimpleForm_SimpleForm_Longitude(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/Longitude";
-        return (android.view.View) solo.getView((Object) ref);
-    }
-
-    /*
-        Type: input
-        Ref:  Simple_Form/Simple_Form/Name
-     */
-    public static EditText get_SimpleForm_SimpleForm_Name(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/Name";
+    public static EditText get_Control_IDs_NextPointID(Solo solo) {
+        String ref = "Control/IDs/Next_Point_ID";
         return (android.widget.EditText) solo.getView((Object) ref);
     }
 
     /*
         Type: input
-        Ref:  Simple_Form/Simple_Form/Northing
+        Ref:  Control/IDs/Next_Polygon_ID
      */
-    public static View get_SimpleForm_SimpleForm_Northing(Solo solo) {
-        String ref = "Simple_Form/Simple_Form/Northing";
+    public static EditText get_Control_IDs_NextPolygonID(Solo solo) {
+        String ref = "Control/IDs/Next_Polygon_ID";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Control/Search/Search_Term
+     */
+    public static EditText get_Control_Search_SearchTerm(Solo solo) {
+        String ref = "Control/Search/Search_Term";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/GControl/Date_Completed
+     */
+    public static EditText get_Feature_GControl_DateCompleted(Solo solo) {
+        String ref = "Feature/GControl/Date_Completed";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/GControl/Description
+     */
+    public static EditText get_Feature_GControl_Description(Solo solo) {
+        String ref = "Feature/GControl/Description";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/GControl/Other_Description
+     */
+    public static EditText get_Feature_GControl_OtherDescription(Solo solo) {
+        String ref = "Feature/GControl/Other_Description";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Main/Accuracy
+     */
+    public static View get_Feature_Main_Accuracy(Solo solo) {
+        String ref = "Feature/Main/Accuracy";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Main/Easting
+     */
+    public static View get_Feature_Main_Easting(Solo solo) {
+        String ref = "Feature/Main/Easting";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Main/ID
+     */
+    public static EditText get_Feature_Main_ID(Solo solo) {
+        String ref = "Feature/Main/ID";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Main/Latitude
+     */
+    public static View get_Feature_Main_Latitude(Solo solo) {
+        String ref = "Feature/Main/Latitude";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Main/Longitude
+     */
+    public static View get_Feature_Main_Longitude(Solo solo) {
+        String ref = "Feature/Main/Longitude";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Main/Northing
+     */
+    public static View get_Feature_Main_Northing(Solo solo) {
+        String ref = "Feature/Main/Northing";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Main/Note
+     */
+    public static EditText get_Feature_Main_Note(Solo solo) {
+        String ref = "Feature/Main/Note";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Shape/Shape_Note/Label
+     */
+    public static EditText get_Shape_ShapeNote_Label(Solo solo) {
+        String ref = "Shape/Shape_Note/Label";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Shape/Shape_Note/Line_ID
+     */
+    public static View get_Shape_ShapeNote_LineID(Solo solo) {
+        String ref = "Shape/Shape_Note/Line_ID";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Shape/Shape_Note/Note
+     */
+    public static EditText get_Shape_ShapeNote_Note(Solo solo) {
+        String ref = "Shape/Shape_Note/Note";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Shape/Shape_Note/Point_ID
+     */
+    public static View get_Shape_ShapeNote_PointID(Solo solo) {
+        String ref = "Shape/Shape_Note/Point_ID";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Shape/Shape_Note/Polygon_ID
+     */
+    public static View get_Shape_ShapeNote_PolygonID(Solo solo) {
+        String ref = "Shape/Shape_Note/Polygon_ID";
         return (android.view.View) solo.getView((Object) ref);
     }
 
     /*
         Type: list
-        Ref:  Control/Simple_Forms/Existing_Simple_Forms
+        Ref:  Control/Search/Entity_List
      */
-    public static View get_Control_SimpleForms_ExistingSimpleForms(Solo solo) {
-        String ref = "Control/Simple_Forms/Existing_Simple_Forms";
+    public static View get_Control_Search_EntityList(Solo solo) {
+        String ref = "Control/Search/Entity_List";
         return (android.view.View) solo.getView((Object) ref);
     }
 
     /*
         Type: list
-        Ref:  User/User/User_List
+        Ref:  User_List/User_List/User_List
      */
-    public static View get_User_User_UserList(Solo solo) {
-        String ref = "User/User/User_List";
+    public static View get_UserList_UserList_UserList(Solo solo) {
+        String ref = "User_List/User_List/User_List";
         return (android.view.View) solo.getView((Object) ref);
     }
 
@@ -190,6 +451,15 @@ public class ModuleUtil extends ModuleHelper {
      */
     public static View get_Control_Map_Map(Solo solo) {
         String ref = "Control/Map/Map";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: webview
+        Ref:  User_List/Manual/Manual
+     */
+    public static View get_UserList_Manual_Manual(Solo solo) {
+        String ref = "User_List/Manual/Manual";
         return (android.view.View) solo.getView((Object) ref);
     }
 }
